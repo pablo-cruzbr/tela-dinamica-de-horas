0c0b0e4a7fbc08f3912c4b12de9d830267e0dbdd
PaPablo Cruz <pablo.cruz3360@gmail.com>
2024-01-11 21:29:41 -0300

    Atualizado 11/01

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ function carregar() {
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
+
+    //1 - COMENTE var hora = data.getHours(); para testar as variações.
+    var hora = data.getHours();
+    
+    //2 - ESCOLHA A HORA ABAIXO:
+    //var hora = 21
  
-    //var hora = data.getHours();
     msg.innerHTML = `Agora são ${hora} horas.`; 
 
     if (hora >= 0 && hora < 12){
@@ -16,7 +21,7 @@ function carregar() {
         document.body.style.background = '#e85d04'
     }else{
         //boa noite
-        img.src = "img/noite.png.png"
+        img.src = "img/noite.png"
         document.body.style.background = '#3f37c9'
     }
 }
